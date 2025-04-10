ebe18f4b0eb2d735be51977e234bc94d2bd862c0
Varelaso32 <samyst2013@hotmail.com>
2025-04-09 20:38:22 -0500

    ✨ feat: Add Migrations
    Se crean las migraciones de la BD

---
+++ Library_Management_System/database/migrations/2025_04_10_012349_create_books.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('título');
+            $table->string('autor');
+            $table->string('isbn')->unique();
+            $table->date('año_publicación');
+            $table->string('editorial');
+
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
