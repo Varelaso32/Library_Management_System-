922cfd39ca22e03853ae000766b294f18cc8a0e7
Varelaso32 <samyst2013@hotmail.com>
2025-04-09 22:15:39 -0500

    ✨ feat: Prestamos Delete
    Se agrega en el controller el destroy para poder eliminar prestamos

--- Library_Management_System/app/Http/Controllers/PrestamosController.php
+++ Library_Management_System/app/Http/Controllers/PrestamosController.php
@@ -126,4 +126,26 @@ class PrestamosController extends Controller
 
         return view('prestamos.index', ['prestamos' => $prestamos]);
     }
+
+    public function destroy(string $id)
+    {
+        $prestamo = Prestamos::findOrFail($id);
+        $prestamo->delete();
+
+        $prestamos = DB::table('prestamos')
+            ->join('books', 'prestamos.libro_id', '=', 'books.id')
+            ->join('readers', 'prestamos.lector_id', '=', 'readers.id')
+            ->select(
+                'prestamos.*',
+                'books.id as book_id',
+                'books.título as book_title',
+                'books.autor as book_author',
+                'readers.id as reader_id',
+                'readers.nombre as reader_name',
+                'readers.email as reader_email'
+            )
+            ->get();
+
+        return view('prestamos.index', ['prestamos' => $prestamos]);
+    }
 }
